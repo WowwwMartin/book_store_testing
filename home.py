0b6b71f0fa65609c3e800fb476e0a3bd8885b87f
Evgeny Kuyat <kuyat85@gmail.com>
2023-02-27 19:54:43 +0300

    Testing book store

---
+++ home.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+# ДОБАВЛЕНИЕ КОММЕНТАРИЯ
+# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+# driver.maximize_window()
+# driver.get("http://practice.automationtesting.in/")
+# driver.execute_script("window.scrollBy(0, 700);")
+# driver.find_element_by_css_selector(".post-160>.button").click()
+# driver.find_element_by_class_name("reviews_tab").click()
+# driver.find_element_by_class_name("star-5").click()
+# review=driver.find_element_by_id("comment")
+# review.send_keys("Nice book!")
+# name=driver.find_element_by_id("author")
+# name.send_keys("Martin Bush")
+# email=driver.find_element_by_id("email")
+# email.send_keys("martinbush@gmail.com")
+# driver.find_element_by_class_name("submit").click()
---
+++ login_registration.py
@@ -0,0 +1,45 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+# РЕГИСТРАЦИЯ
+# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+# driver.maximize_window()
+# driver.get("http://practice.automationtesting.in/")
+# menu=driver.find_element_by_id("menu-item-50")
+# menu.click()
+# register_mail=driver.find_element_by_id("reg_email")
+# register_mail.send_keys("EVK@gmail.com")
+# register_password=driver.find_element_by_id("reg_password")
+# register_password.send_keys("!!!MartiN!@#$%^&*()16081985!!!")
+# time.sleep(3)
+# submit=driver.find_element_by_xpath("//input[@name='register']")
+# time.sleep(3)
+# submit.click()
+# time.sleep(3)
+
+# ЛОГИН
+# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+# driver.maximize_window()
+# driver.get("http://practice.automationtesting.in/")
+# menu = driver.find_element_by_id("menu-item-50")
+# menu.click()
+# log_username = driver.find_element_by_id("username")
+# log_username.send_keys("EVK@gmail.com")
+# log_password = driver.find_element_by_id("password")
+# log_password.send_keys("!!!MartiN!@#$%^&*()16081985!!!")
+# time.sleep(3)
+# login = driver.find_element_by_class_name("login>:nth-child(3)>:nth-child(3)")
+# login.click()
+# logout = driver.find_element_by_class_name("woocommerce-MyAccount-navigation>:nth-child(1)>:nth-child(6)>:nth-child(1)")
+# logout_text = logout.text
+# assert "Logout" in logout_text
+# url = driver.current_url
+# print("Элемент LOGOUT обнаружен на странице:", url)
+
+
+
+
+
+
+
